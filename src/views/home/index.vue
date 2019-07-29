2e4ff752bd822e667c33a2a597f2cb64e8811b97
jinguifeng <1227886331@qq.com>
2019-07-29 18:14:01 +0800

    基本结构+按需加载

---
+++ src/views/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <Button type="primary">Primary</Button>
+  </div>
+</template>
+
+<script>
+import { Button } from 'ant-design-vue';
+export default {
+  components: {
+    Button
+  }
+}
+</script>
+

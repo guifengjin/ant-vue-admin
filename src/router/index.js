15649708c8f3ea3cacfb1c0554b762b0d3be4c83
jinguifeng <1227886331@qq.com>
2019-07-29 18:39:24 +0800

    ----

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,22 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: 'user',
+      redirect: '/user',
+      children: [{
+        path: '/',
+        component: () => import('@/views/user')
+      },{
+        path: 'login',
+        name: 'login',
+        component: () => import('@/views/user/login')
+      },{
+        path: 'register',
+        name: 'register',
+        component: () => import('@/views/user/register')
+      }]
+    },
+    {
       path: '/',
       name: 'home',
       component: () => import('@/views/home') // 懒加载
